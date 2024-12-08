71a7e703a09c146885836e744e6ec52ee24c1ff7
Tomatote <vargastomas2003@gmail.com>
2024-12-08 19:16:29 -0300

    initial commit

---
+++ Models/Tarea.cs
@@ -0,0 +1,21 @@
+namespace projectef.Models
+{
+
+    public enum Prioridad
+    {
+        Baja,
+        Mediana,
+        Alta
+    }
+
+    public class Tarea
+    {
+        public Guid TareaId { get; set; }
+        public Guid CategoriaId { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public Prioridad Prioridad { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
