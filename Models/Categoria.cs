1e9941ff79decb0e345b5f3d7799248c16e1634d
Tomatote <vargastomas2003@gmail.com>
2024-12-08 19:23:12 -0300

    Task & Categories Constraints

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,8 +1,14 @@
 namespace projectef.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class Categoria
 {
-    public Guid CategoriaId { get; set; }
+    [Key]
+    public Guid CategoriaId { get; set; }}
+    
+    [Required]
+    [MaxLength(150)]
     public string Nombre { get; set; }
     public string Descripcion { get; set; }
     public virtual ICollection<Tarea> Tareas { get; set; }
--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace projectef.Models
 {
 
@@ -10,12 +13,22 @@ namespace projectef.Models
 
     public class Tarea
     {
+        [Key]
         public Guid TareaId { get; set; }
+
+        [ForeignKey("CategoriaId")]
         public Guid CategoriaId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public Prioridad Prioridad { get; set; }
         public DateTime FechaCreacion { get; set; }
+
         public virtual Categoria Categoria { get; set; }
+
+        [NotMapped]
+        public string Resumen { get; set; }
     }
 }
