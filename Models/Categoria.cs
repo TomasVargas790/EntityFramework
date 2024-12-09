8b26e435afa8ce6bce6e57d3378b6c8bdee381dd
Tomatote <vargastomas2003@gmail.com>
2024-12-08 23:02:13 -0300

    FEAT GET/api/tareas

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,6 +1,7 @@
 namespace projectef.Models;
 
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 public class Categoria
 {
@@ -8,5 +9,6 @@ public class Categoria
     public string Nombre { get; set; }
     public string Descripcion { get; set; }
     public int Peso { get; set; }
+    [JsonIgnore]
     public virtual ICollection<Tarea> Tareas { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using projectef;
+using projectef.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,16 @@ app.MapGet("/dbconexion", ([FromServices] TareasContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
     return Results.Ok($"Base de datos en memoria: {dbContext.Database.IsInMemory()}");
+});
+
+app.MapGet("/api/tareas", ([FromServices] TareasContext dbContext) =>
+{
+    return Results.Ok
+    (
+        dbContext.Tareas
+            .Include(t => t.Categoria)
+            .Where(t => t.Prioridad == Prioridad.Baja)
+    );
 });
 
 app.Run();
